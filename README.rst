f1e55389d7eaaf2e1e14164a024e89949d3fbfaa
Martin Ortbauer <mortbauer@gmail.com>
2015-07-30 12:29:12 +0200

    initialized git repo, relU working

---
+++ README.rst
@@ -0,0 +1,1 @@
+from: http://www.cfd-online.com/Forums/openfoam-solving/71277-how-derive-relative-velocity-mrfsimplefoam.html
---
+++ relU.C
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    relU
+
+Description
+    Calculate the relative velocity in a moving reference frame.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvIOoptionList.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    timeSelector::addOptions();
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+
+    instantList timeDirs = timeSelector::select0(runTime, args);
+
+    #include "createMesh.H"
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+
+        Info<< "Time = " << runTime.timeName() << endl;
+
+        IOobject Uheader
+        (
+            "U",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ
+        );
+
+        // Check if U exists
+        if (Uheader.headerOk())
+        {
+            mesh.readUpdate();
+
+            Info<< "    Reading U" << endl;
+            volVectorField U(Uheader, mesh);
+
+            surfaceScalarField phi
+            (
+                IOobject
+                (
+                    "phi",
+                    runTime.timeName(),
+                    mesh,
+                    IOobject::NO_READ
+                ),
+                fvc::interpolate(U) & mesh.Sf()
+            );
+
+            #include "createFvOptions.H"
+
+            Info<< "    Calculating relU" << endl;
+            fvOptions.makeRelative(phi);
+            volVectorField relU
+            (
+                IOobject
+                (
+                    "relU",
+                    runTime.timeName(),
+                    mesh,
+                    IOobject::NO_READ
+                ),
+                fvc::reconstruct(phi)
+            );
+            relU.write(); 
+        }
+        else
+        {
+            Info<< "    No U" << endl;
+        }
+
+        Info<< endl;
+    }
+
+    return 0;
+}
+
+
+// ************************************************************************* //
